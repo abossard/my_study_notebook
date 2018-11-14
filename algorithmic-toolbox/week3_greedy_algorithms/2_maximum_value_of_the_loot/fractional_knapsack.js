98ae7b8138bc36a7049e4394bd7430716af8d9cc
Andre Bossard <abossard@gmail.com>
2018-11-14 21:58:07 +0100

    hihihihihi

--- algorithmic-toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.js
+++ algorithmic-toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.js
@@ -12,22 +12,42 @@ let count = 0;
 let sackSize = 0;
 let pairs = []
 
+function knapsack(pairs, size) {
+    let sorted = pairs.slice().map(item=>[item[1], item[0]/item[1]])
+    let rest = size
+    let i = 0
+    let value = 0
+    sorted.sort((a, b) => b[1] - a[1])
+    while(rest > 0 && i < sorted.length) {
+        let item = sorted[i]
+        let takeWeight = Math.min(item[0], rest)
+        rest -= takeWeight
+        value += takeWeight * item[1]
+        i++
+    }
+    return value;
+}
+
 function readLine (line) {
     let values = line.split(' ').map(value => parseInt(value, 10))
     if(count==0 && sackSize==0) {
         count = values[0];
         sackSize = values[1];
     } else {
-        pairs.push([values[0], values[1], values[0]/values[1]])
+        pairs.push([values[0], values[1]])
     }
     if(pairs.length == count) {
-        // execute
+        console.log(knapsack(pairs, sackSize))
         process.exit();
     }
 }
   
 function main() {
-    console.log(change(39));
-    process.exit()
+    console.log(knapsack([
+        [60, 20],
+        [100, 50],
+        [120, 30],
+    ], 50))
+      process.exit()
 }
-main()
+//main()

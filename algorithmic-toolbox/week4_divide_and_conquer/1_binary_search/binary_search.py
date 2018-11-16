f45eedbf790c2f6e2f5eb8c8ef124c19a3afeacd
Andre Bossard <abossard@gmail.com>
2018-11-17 00:43:52 +0100

    ah crap

--- algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -9,19 +9,15 @@ def binary_search(haystack, needle):
     if haystack[middle] == needle:
         return middle
     elif needle < haystack[middle]:
-        return binary_search(haystack[:middle - 1], needle)
+        return binary_search(haystack[:middle], needle)
     else: 
-        return binary_search(haystack[middle+1:], needle)
-
-
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
-    return -1
+        return binary_search(haystack[middle + 1:], needle)
 
 if __name__ == '__main__':
-    haystack = list(map(int, sys.stdin.read().split()))[1:]
-    needles = list(map(int, sys.stdin.read().split()))[1:]
+    #input = sys.stdin.read()
+    #data = list(map(int, input.split()))
+    #n = data[0]
+    haystack = [1,2,3,4,5] #data[1 : n + 1]
+    needles = [4,5]#data[n + 2:]
     for needle in needles:
         print(binary_search(haystack, needle), end = ' ')

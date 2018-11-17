3c5f9a8d7266d6b1fcaeb93e1695164ff5bfa0cd
Andre Bossard <abossard@gmail.com>
2018-11-17 01:58:54 +0100

    so, is it better

--- algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,23 +1,22 @@
 # Uses python3
 import sys
 
-def binary_search(haystack, needle):
-    start, end = 0, len(haystack)
+def binary_search(haystack, start, end, needle):
     if end == 0:
         return -1
-    middle = int((end - start)/2)
+    middle = int(start + (end - start)/2)
     if haystack[middle] == needle:
         return middle
     elif needle < haystack[middle]:
-        return binary_search(haystack[:middle], needle)
+        return binary_search(haystack, start, middle, needle)
     else: 
-        return binary_search(haystack[middle + 1:], needle)
+        return binary_search(haystack, middle + 1, end, needle)
 
 if __name__ == '__main__':
-    #input = sys.stdin.read()
-    #data = list(map(int, input.split()))
-    #n = data[0]
-    haystack = [1,2,3,4,5] #data[1 : n + 1]
-    needles = [4,5]#data[n + 2:]
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n = data[0]
+    haystack = data[1 : n + 1]
+    needles = data[n + 2:]
     for needle in needles:
-        print(binary_search(haystack, needle), end = ' ')
+        print(binary_search(haystack, 0, len(haystack) - 1, needle), end = ' ')

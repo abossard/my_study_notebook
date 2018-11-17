572a8b8825ef547eceaa1c2fa0e12fa6838dd985
Andre Bossard <abossard@gmail.com>
2018-11-17 02:10:31 +0100

    puh finally

--- algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -2,13 +2,13 @@
 import sys
 
 def binary_search(haystack, start, end, needle):
-    if end == 0:
+    if start > end:
         return -1
     middle = int(start + (end - start)/2)
     if haystack[middle] == needle:
         return middle
     elif needle < haystack[middle]:
-        return binary_search(haystack, start, middle, needle)
+        return binary_search(haystack, start, middle - 1, needle)
     else: 
         return binary_search(haystack, middle + 1, end, needle)
 
@@ -18,5 +18,9 @@ if __name__ == '__main__':
     n = data[0]
     haystack = data[1 : n + 1]
     needles = data[n + 2:]
+
+    #haystack = [1, 5, 8, 12, 13]
+    #needles = [8, 1, 23, 1, 11]
+
     for needle in needles:
         print(binary_search(haystack, 0, len(haystack) - 1, needle), end = ' ')
